8d6667039e1005dbdc57f8681dbb93627771f726
gokernel2017 <31101188+gokernel2017@users.noreply.github.com>
2017-09-19 15:28:19 -0300

    Add files via upload

--- summer.js
+++ summer.js
@@ -4,6 +4,8 @@
 //
 //-----------------------------------------------
 //
+// onmouseover, onmouseout
+//
 var id=' ';
 var idCount=0;
 var isTouch=false;
@@ -11,6 +13,8 @@ var isTouch=false;
 var offset = [0,0];
 var isDown = false;
 
+var mx, my; // mouse_x, mouse_y
+var main_widget;
 
 function so_Init () {
   isTouch = (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
@@ -18,9 +22,38 @@ function so_Init () {
     alert ("INIT ERRO: tag: <body> or tag: <link rel='stylesheet' href='garden.css' /> not found");
     return 0;
   }
+
+  document.onmousemove = function(event) {
+    //-------------------------------------------
+    // isDown == true
+    // on click in the window title bar.
+    //-------------------------------------------
+    if (isDown) {
+      if (isTouch) { // mobile
+        var t = event.changedTouches[0];
+        event.preventDefault();
+        //
+        // change widget position ( style value )
+        //
+        mx = t.clientX;
+        my = t.clientY;
+        main_widget.style.left = (t.clientX + offset[0]) + 'px';
+        main_widget.style.top  = (t.clientY + offset[1]) + 'px';
+      } else { // no mobile
+        event.preventDefault();
+        //
+        // change widget position ( style value )
+        //
+        mx = event.clientX;
+        my = event.clientY;
+        main_widget.style.left = (event.clientX + offset[0]) + 'px';
+        main_widget.style.top  = (event.clientY + offset[1]) + 'px';
+      }
+    }//: if (isDown)
+  }//: document.onmousemove = function(event)
+
   return 1;
 }
-
 
 function so_NewWindow(x,y,w,h,txt) {
   var _x_, _y_, _w_, _h_; // to restore saved window size
@@ -79,14 +112,14 @@ function so_NewWindow(x,y,w,h,txt) {
     o.addEventListener('touchstart', function(event) {
       var t = event.changedTouches[0];
 
-      if (t.clientY-this.offsetTop < 30) {
-          isDown = true;
+      if (t.clientY-this.offsetTop < 30) { // click in title bar
+
+          isDown = true; //<<<<<<< enable here >>>>>>>
+
           offset = [
             this.offsetLeft - t.clientX,
             this.offsetTop  - t.clientY
           ];
-          // invisible title bar
-          b.style.display = 'none';
       }
       var array = document.getElementsByTagName("widget");
 
@@ -97,10 +130,14 @@ function so_NewWindow(x,y,w,h,txt) {
           array[i].style.zIndex = 1;
       }
       this.style.zIndex = array.length;
+
+  main_widget = this;
+
     }, true);
 
-    o.addEventListener('touchend', function () { isDown = false; b.style.display = 'block'; }, true);
+    o.addEventListener('touchend', function () { isDown = false; }, true);
 
+/*
     o.addEventListener('touchmove', function(event) {
       var t = event.changedTouches[0];
 
@@ -113,22 +150,21 @@ function so_NewWindow(x,y,w,h,txt) {
         this.style.top  = (t.clientY + offset[1]) + 'px';
       }
     }, true);
+*/
   } else {
     //-------------------------------------------
     // DESKTOP MOUSE EVENT:
     //-------------------------------------------
     //
     o.addEventListener('mousedown', function(event) {
-      //if (event.clientX-this.offsetLeft > 64 && event.clientY-this.offsetTop < 30) {
       if (event.clientY-this.offsetTop < 30) {
-          isDown = true;
+
+          isDown = true; //<<<<<<<  enable here  >>>>>>>
+
           offset = [
             this.offsetLeft - event.clientX,
             this.offsetTop - event.clientY
           ];
-          // invisible title bar
-          b.style.display = 'none';
-        //document.getElementById("main_body").style.display = "none";
       }
       var array = document.getElementsByTagName("widget");
       //
@@ -138,12 +174,16 @@ function so_NewWindow(x,y,w,h,txt) {
           array[i].style.zIndex = 1;
       }
       this.style.zIndex = array.length;
+
+  main_widget = this;
+
     }, true);
 
-    o.addEventListener('mouseup', function() { isDown = false; b.style.display = 'block'; }, true);
+    o.addEventListener('mouseup', function() { isDown = false; }, true);
 
+/*
     o.addEventListener('mousemove', function(event) {
-      if (isDown && event.clientY < this.offsetTop+30) {
+      if (isDown) {// && event.clientY < this.offsetTop+30) {
         event.preventDefault();
         //
         // change widget position ( style value )
@@ -152,7 +192,7 @@ function so_NewWindow(x,y,w,h,txt) {
         this.style.top  = (event.clientY + offset[1]) + 'px';
       }
     }, true);
-
+*/
   }
 
   b.appendChild(bmin);
